e234d72d9927ac18dd72f15f585d4f2c1a2f6633
gompopcorn <gom.nakonamesh@gmail.com>
2021-12-25 11:35:04 +0100

    adding assets without cli

--- bash-files/createCar.sh
+++ bash-files/createCar.sh
@@ -21,6 +21,11 @@ owner=$8
 #               Paths, Addresses and Ports
 ##########################################################
 
+cd $vms_dir
+
+path_orderer_tls_ca="$vms_dir/vm4/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+org_dir="${PWD}/vm${orgNumber}/crypto-config/peerOrganizations/$orgName.example.com"
+
 # select proper $addr_peer0 from environment variables
 select_addr_peer0="addr_${orgName}_peer0"  
 peer0_addr=${!select_addr_peer0}
@@ -30,32 +35,27 @@ selectPeer0Port="port_${orgName}_peer0"
 peer0_port=${!selectPeer0Port}
 
 # select proper $path_org_users_in_cli from environment variables
-select_path_org_users_in_cli="path_${orgName}_users_in_cli"  
-path_org_users_in_cli=${!select_path_org_users_in_cli}
+select_path_org_users="path_${orgName}_users"  
+path_org_users=${!select_path_org_users}
 
 
-CORE_PEER_MSPCONFIGPATH="$path_org_users_in_cli/$username@$orgName.example.com/msp"
-CORE_PEER_ADDRESS=$peer0_addr:$peer0_port
-VERSION="1"
+export FABRIC_CFG_PATH=$fabric_samples_dir/config
+export CORE_PEER_MSPCONFIGPATH=$org_dir/users/$username@$orgName.example.com/msp
+# export CORE_PEER_ADDRESS=$peer0_addr:$peer0_port
+export ORDERER_CA=$path_orderer_tls_ca
+export VERSION="1"
 
+export CORE_PEER_LOCALMSPID="Org${orgNumber}MSP"
 
 ##########################################################
 #               Add the asset to the Ledger
 ##########################################################
 
-cat << EOF | docker exec --interactive cli bash
+peer chaincode invoke -o $ip_orderer_server:$port_orderer1 --ordererTLSHostnameOverride $addr_orderer1 \
+--tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $channelName -n $chaincodeName \
+--peerAddresses localhost:$port_org1_peer0 --tlsRootCertFiles $path_tlsRootCertFiles_org1 \
+-c '{"function": "createCar", "Args":["'$id'", "'$make'", "'$model'", "'$colour'", "'$owner'"]}'
 
-    export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
-    export CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS
-    export ORDERER_CA=$path_orderer_tls_ca
-    export VERSION=$VERSION
-
-    peer chaincode invoke -o $addr_orderer1:$port_orderer1 --ordererTLSHostnameOverride $addr_orderer1 \
-    --tls --cafile $path_orderer_msp_ca -C $channelName -n $chaincodeName \
-    --peerAddresses $addr_org1_peer0:$port_org1_peer0 --tlsRootCertFiles $path_tlsRootCertFiles_org1 \
-    -c '{"function": "createCar", "Args":["$id", "$make", "$model", "$colour", "$owner"]}'
-
-EOF
 
 
 # cat << EOF | docker exec --interactive cli bash
@@ -68,8 +68,6 @@ EOF
 #     peer chaincode invoke -o $addr_orderer1:$port_orderer1 --ordererTLSHostnameOverride $addr_orderer1 \
 #     --tls --cafile $path_orderer_msp_ca -C $channelName -n $chaincodeName \
 #     --peerAddresses $addr_org1_peer0:$port_org1_peer0 --tlsRootCertFiles $path_tlsRootCertFiles_org1 \
-#     --peerAddresses $addr_org2_peer0:$port_org2_peer0 --tlsRootCertFiles $path_tlsRootCertFiles_org2 \
-#     --peerAddresses $addr_org3_peer0:$port_org3_peer0 --tlsRootCertFiles $path_tlsRootCertFiles_org3 \
 #     -c '{"function": "createCar", "Args":["$id", "$make", "$model", "$colour", "$owner"]}'
 
 # EOF
